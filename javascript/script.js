a73b270b3968c34209d859cfff7989bbd1209e31
jotibap <jotibapatil457@gmail.com>
2025-12-04 15:59:36 +0530

    flexbox-Grid

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+console.log("javascript working successfully");
+
+document.writeln("Hello!!!!!!!!!");
+
+let name = prompt("enter your name");
+console.log("your name is: " + name);
+
+let age = Number(prompt("Enter your age"));
+console.log("your age is " + age);
